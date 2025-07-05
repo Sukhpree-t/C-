dd93657642af6bad8f423a22995a0cd99ec05280
Earthian <sukhpreet08072005@gmail.com>
2025-07-05 15:56:44 +0530

    changes Bino_coeff.cpp

--- Function/Bino_coeff.cpp
+++ Function/Bino_coeff.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
 using namespace std;
 
+int factorial(int n) {
+    if (n <= 0) {
+        cout << "Invalid input: Factorial is not defined for negative numbers." << endl;
+        return -1;
+    }
+    int fact = 1;
+    for(int i=1; i<=n; i++) {
+        fact*=i;
+    }   
+}
+
 int Bino_coeff(int n, int r) {
     if (r > n) {
         cout << "Invalid input: r cannot be greater than n." << endl;
         return 0;
     }
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_n_r = factorial(n-r);
 
-    int fact_n=1, fact_r=1, fact_n_r=1;
-
-    for(int i=1; i<=n; i++) {
-        fact_n*=i;
-    }
-    for(int i=1; i<=r; i++) {
-        fact_r*=i;
-    }
-    for(int i=1; i<=n-r; i++) {
-        fact_n_r=fact_n_r*i;
-    }
-
-    int binomial_coefficient = fact_n / (fact_r * fact_n_r);
-    
-    return binomial_coefficient;
+    return fact_n / (fact_r * fact_n_r);
 }
 
 int main() {
@@ -34,7 +34,7 @@ int main() {
     int result = Bino_coeff(n,r);
 
     if(result == 0 && r > n) {
-        return 1; // Exit with error code if input is invalid
+        return 1;
     }
     cout << "The binomial coefficient C(" << n << ", " << r << ") is: " << result << endl;
     return 0;
